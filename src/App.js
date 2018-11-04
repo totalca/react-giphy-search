ea797aa66d43578a2c470b71f27554d3fbff0fca
Nicolae Totalca <totalca@yahoo.com>
2018-11-04 15:36:23 +0000

    Display the title and images

--- src/App.js
+++ src/App.js
@@ -10,17 +10,33 @@ class App extends Component {
     limit:3
   }
   componentDidMount() {
-    axios.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=fhruhErb7kOixSYUM2EV916C9qVt2wiL&limit=5")
+    axios.get("http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=fhruhErb7kOixSYUM2EV916C9qVt2wiL&limit=5")
       .then(({data}) => {
         console.log(data)
         this.setState({list:data.data})
       })
   }
+
+  renderList = () => {
+    return this.state.list.map((item, key) => 
+    <li key={key}>
+      <h2>{item.title}</h2>
+      <img 
+        src={item.images.original.url} 
+        width={item.images.original.width} 
+        height={item.images.original.height} 
+        alt={item.title}
+      />
+    </li>)
+  }
+
   render() {
     return (
       <div className="App">
         Giphy Search
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+        <ul>
+          {this.renderList()}
+        </ul>
       </div>
     );
   }
